a82c9075b7037fe97d5a9193808476a31dfbd4e2
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-23 17:14:11 +0000

    compound assignment

---
+++ 01_basics/06_compound_assignment.py
@@ -0,0 +1,17 @@
+x = 10
+
+# addition assignment
+x += 5
+print(x)
+
+# substraction assignment
+x -= 3
+print(x)
+
+# multiplication assignment
+x *= 2
+print(x)
+
+# division assignment
+x /= 4
+print(x)
