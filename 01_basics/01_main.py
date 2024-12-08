d345f7db8de2ec74b680424877a9fda8deceabde
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-12-08 13:11:37 +0000

    delete

--- 01_basics/01_main.py
+++
@@ -1,7 +0,0 @@
-print("saksham")
-
-for i in range(1, 10):
-    print(i)
-
-if(True):
-    print('saksham')
--- 01_basics/07_increment_operator.py
+++
@@ -1,9 +0,0 @@
-x = 5
-
-x += 1 # equivalent to x = x + 1
-print(x)
-
-y = 10
-
-y -= 1 # equivalent to y = y - 1
-print(y)
